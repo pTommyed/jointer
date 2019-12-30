9e917a70b8e96e2b4ada331a6e49bad291dd34ca
pTommyed <35973997+pTommyed@users.noreply.github.com>
2019-12-30 14:24:23 +0100

    30.12.19

--- Arduino_moduly_kody_K1_K2_K3 - GIT-nutne otestovat/K3/JointerCANBridge1.9.2.1.1_9xFIX/JointerCANBridge1.9.2.1.1_9xFIX.ino
+++ Arduino_moduly_kody_K1_K2_K3 - GIT-nutne otestovat/K3/JointerCANBridge1.9.2.1.1_9xFIX/JointerCANBridge1.9.2.1.1_9xFIX.ino
@@ -1,46 +1,74 @@
-/////////////////////////////////////////////////////////////////////
-/*    - K2 -     */
-/*verze bez bufru příchozích zpráv a překlad realizován odečítáním adres*/
-/* verze pro K2*/
+//####################################################
+//#    KLOUBAK: JointerCANBridge1.9.2.1.1_9xFIX
+//#    Copyright (c) 2019 ROBOTIKA
+//#    programmed by: Tomas Picha, Jan Kaderabek
+//####################################################
 
-/////////////////////////////////////////////////////////////////////
-/*{přijatá hodntoa,přeposílaná hodntota} -- to co se má jen přeposílat APU*/
-/*int CANAdressReceiveTable[4][2]={{2305,145},
-                                 {2306,146},
-                                 {2307,147},
-                                 {2308,148}}; 
-/*{přijatá hodntoa,přeposílaná hodntota} -- to co se má posílat vESCum*/
-/*int CANAdressSendTable[12][2]={{17,257},
-                              {18,258},
-                              {19,259},
-                              {20,260},
-                              {33,513}, //neni pozmeneno !!! 
-                              {34,514}, //neni pozmeneno !!!
-                              {35,515},
-                              {36,516},
-                              {49,769},
-                              {50,770},
-                              {51,771},
-                              {52,772}};*/
-/////////////////////////////////////////////////////////////////////
 
+// DESCRTIPTION: -  version for K2 and K3 robots 
+//               -  no tested
+//               -  20 Hz - sampling frequency 
+//               -  version without incoming message buffer and translation realized by address subtraction
+//               -  version with changed CountCycle()
+// HW: Aruino Due, MCP2515+TJA1050 CAN module
+
+//########################################################################################################
+//# Tables for CAN address conversion
+//################################################
+//################################################
+//# Tabel-receiving data from vescs sending to apu
+//################################################
+//# received address_(from_vesc_adress)     converted address(to_apu)
+//#     2305[dec]-0x901[hex]                   145[dec]-0x91[hex]
+//#     2306[dec]-0x902[hex]                   146[dec]-0x92[hex]
+//#     2307[dec]-0x903[hex]                   147[dec]-0x93[hex]
+//#     2308[dec]-0x904[hex]                   148[dec]-0x94[hex]
+//#     2309[dec]-0x905[hex]                   149[dec]-0x95[hex]
+//#     2310[dec]-0x906[hex]                   150[dec]-0x96[hex]
+//################################################
+//################################################
+//# Table-receiving data from apu sending to vescs
+//################################################
+//# received address_(from_apu)     converted address(to_vescs)
+//#     17[dec]-0x11[hex]              257[dec]-0x101[hex]
+//#     18[dec]-0x12[hex]              258[dec]-0x102[hex]
+//#     19[dec]-0x13[hex]              259[dec]-0x103[hex]
+//#     20[dec]-0x14[hex]              260[dec]-0x104[hex]
+//#     21[dec]-0x15[hex]              261[dec]-0x105[hex]
+//#     22[dec]-0x16[hex]              262[dec]-0x106[hex]
+//#     33[dec]-0x21[hex]              513[dec]-0x201[hex]
+//#     34[dec]-0x22[hex]              514[dec]-0x202[hex]
+//#     35[dec]-0x23[hex]              515[dec]-0x203[hex]
+//#     36[dec]-0x24[hex]              516[dec]-0x204[hex]
+//#     37[dec]-0x25[hex]              517[dec]-0x205[hex]
+//#     38[dec]-0x26[hex]              518[dec]-0x206[hex]
+//#     49[dec]-0x31[hex]              769[dec]-0x301[hex]
+//#     50[dec]-0x32[hex]              770[dec]-0x302[hex]
+//#     51[dec]-0x33[hex]              771[dec]-0x303[hex]
+//#     52[dec]-0x34[hex]              772[dec]-0x304[hex]
+//#     53[dec]-0x35[hex]              773[dec]-0x305[hex]
+//#     54[dec]-0x36[hex]              774[dec]-0x306[hex]
+//########################################################################################################
+
+/*----------------------- DEPENDENCES ----------------------------------*/
 #include <mcp_can.h>
 #include <DueTimer.h>
 
+/*----------------------- DEFINITION -----------------------------------*/
 const int stop_led = 11;  // RED
 const int break_led = 12; // BLUE
 const int ledPin = 10;    // GREEN
 const int Syrena = 8;
 
-byte stop_xbee = 22; // for K2
+//byte stop_xbee = 22;
 byte StopButton = 24;
-byte control_break = 26; // for K2
+byte control_break = 26;
+
+MCP_CAN CAN(2);
 
 int CANvescID[4] = {1,2,3,4};  // ID jednotlivých motorů
 byte currentREVERS[4]={0,0,250,0}; // brzdící proud  
 byte current_no_brake[4]={0,0,0,0}; // no brake current  
-
-MCP_CAN CAN(2);
 
 bool STOpSTARt = true; //pokud je nastaven na true je stop
 bool digitRead = false;
@@ -54,51 +82,33 @@ int  CANmessageID;
 unsigned char len = 0;
 byte buf[8];
 
+/*----------------------- SETUP ----------------------------------------*/
 void setup() {
  
-  Serial.begin(250000);
-
-  pinMode(ledPin, OUTPUT);
-  pinMode(Syrena,OUTPUT); //syrena
-  pinMode(stop_led, OUTPUT);
-  pinMode(break_led, OUTPUT);
-
+  serial_initial();
   
-  digitalWrite(ledPin, LOW);
-  digitalWrite(Syrena, LOW);
+  pin_mode_initial();
+  pin_out_setting();
+  
   ButtonRead();
   brake_button_read();
 
-  pinMode(StopButton,INPUT_PULLUP);
-  pinMode(control_break,INPUT_PULLUP);
-
-  Timer3.attachInterrupt(TimerInterrupt);
-  Timer4.attachInterrupt(BeepDown);
+  timer3_4_initial();
    
-  Serial.println("Setup can..");
-  while(CAN.begin(CAN_500KBPS, MCP_8MHz) != CAN_OK){
-    digitalWrite(stop_led, HIGH);
-    Serial.print(".");
-    BeepUP();
-    delay(1000);
-  }
-  Serial.print("\nCAN init ok!!\r\n");
+  CAN_initial();
+  
   LEDIndicator ();
   BeepUP();
    
-  while(Timmer4End != true){
-    delay(100);
-   // pro jine verye aruduino }jinak nepreklada]  
-  CAN.init_Mask(0, 1, 0x00000910); //CAN.init_Mask(0, 1, 0x06FF0000);
-  CAN.init_Filt(0, 1, 0x00000901);
-  CAN.init_Filt(1, 1, 0x00000902);
-  CAN.init_Filt(2, 1, 0x00000903);
-  CAN.init_Filt(3, 1, 0x00000904);
-  }
+  waiting_for_timer4_overflow();
+ 
+  CAN_Mask_initial();
+  CAN_filters_initial();
 
   Timer3.start(50000); // Calls every 50 mikros (20 Hz)
 }
 
+/*----------------------- LOOP ------------------------------------------*/
 void loop() {
   ButtonRead();
   brake_button_read();
